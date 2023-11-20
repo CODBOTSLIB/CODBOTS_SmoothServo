67cf92ae576396a35f951a2042e0988b76a1a625
Dileepa Jayawardena <me.dreamwalker@gmail.com>
2023-11-20 22:59:54 +0530

    demo app div

--- CODBOTS_Servo.cpp
+++ CODBOTS_Servo.cpp
@@ -32,7 +32,7 @@ bool CODBOTS_Servo::write(int angle)
         }
 
         write_time = millis();
-        target_time = abs(c_angle - angle) * 2;
+        target_time = abs(c_angle - angle) * 10;
 
         c_angle = angle;
     }
